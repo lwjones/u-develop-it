8b82a6c279f1b5cba562659e76c1728240d5cb70
Lawrence Jones <git@lwjon.es>
2022-03-08 15:18:48 -0800

    Connect to database

--- server.js
+++ server.js
@@ -1,10 +1,31 @@
+require('dotenv').config();
 const PORT = process.env.PORT || 3001;
+const mysql = require('mysql2');
 const express = require('express');
 const app = express();
 
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+
+// connect to database
+const db = mysql.createConnection(
+  {
+    host: process.env.HOST,
+    user: process.env.DB_USER,
+    password: process.env.PASSWORD,
+    database: 'election'
+  },
+  console.log('Connected to the election database.')
+);
+
+
+// test connection to database
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+  console.log(rows);
+});
+
 
 
 // Default response for any other request (Not Found)
@@ -15,4 +36,4 @@ app.use((req, res) => {
 
 app.listen(PORT, () => {
   console.log(`  Server running on port ${PORT}`);
-})
+});
