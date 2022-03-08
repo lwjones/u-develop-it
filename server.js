a3882eef3508d2a11be20576221ec7fbcedc716a
Lawrence Jones <git@lwjon.es>
2022-03-08 15:55:31 -0800

    Test connection updating, deleting, creating candidates

--- server.js
+++ server.js
@@ -21,12 +21,40 @@ const db = mysql.createConnection(
 );
 
 
-// test connection to database
-db.query(`SELECT * FROM candidates`, (err, rows) => {
-  console.log(rows);
-});
+// // test connection to database
+// db.query(`SELECT * FROM candidates`, (err, rows) => {
+//   // console.log(rows);
+// });
 
 
+// // GET a single candidate
+// db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+//   // if (err) {
+//   //   console.log(err);
+//   // }
+//   // console.log(row);
+// });
+
+
+// // DELETE a candidate
+// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+//  // if (err) {
+//  //   console.log(err);
+//  // }
+//  // console.log(result);
+// });
+
+// CREATE a candidate
+// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+//              VALUES (?,?,?,?)`;
+// const params = [1, 'Ronald', 'Firbank', 1];
+
+// db.query(sql, params, (err, result) => {
+//   // if (err) {
+//   //   console.log(err);
+//   // }
+//   // console.log(result);
+// })
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
